002e5db36f0ded75728dcff05803f489a78acfdb
Sabbir Hossain Abir <2020-2-60-185@std.ewubd.edu>
2023-12-25 17:58:28 +0600

    Added intents in the all topics

--- app/src/main/java/com/example/onlinetrainingandcourse/AllTopics.java
+++ app/src/main/java/com/example/onlinetrainingandcourse/AllTopics.java
@@ -2,7 +2,9 @@ package com.example.onlinetrainingandcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class AllTopics extends AppCompatActivity {
@@ -12,7 +14,44 @@ public class AllTopics extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_topics);
 
+        homefooterpannel = findViewById(R.id.homefooterpannel);
+        homeHomebtn = findViewById(R.id.homeHomebtn);
+        homeedubtn = findViewById(R.id.homeedubtn);
+        homesrcbtn = findViewById(R.id.homesrcbtn);
+        homesavebtn = findViewById(R.id.homesavebtn);
+        homeprofilebtn = findViewById(R.id.homeprofilebtn);
 
+        homeHomebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllTopics.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+        homeedubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllTopics.this, EducationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homesavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllTopics.this, SaveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        homeprofilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllTopics.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
